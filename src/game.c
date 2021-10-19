a78e813add58b35f2eb6e3e08974e9cc617893d6
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-10-18 21:42:49 -0400

    added 4a really janky implementation for game over

--- src/game.c
+++ src/game.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdio.h>
+#include <signal.h>
 
 #include <game.h>
 #include <gui.h>
@@ -9,12 +11,13 @@ bool updateBlock(Block b, Playfield *p, int dy, int dx, bool drop) {
   return out;
 }
 /**
- * @brief test to see if the block can be shifted on the x axis without leaving the playarea
- * 
- * @param b 
- * @param dx 
- * @return true 
- * @return false 
+ * @brief test to see if the block can be shifted on the x axis without leaving
+ * the playarea
+ *
+ * @param b
+ * @param dx
+ * @return true
+ * @return false
  */
 bool shiftBlockX(Block b, int dx) {
   for (int i = 0; i < b.num_pixels; i++) {
@@ -27,16 +30,16 @@ bool shiftBlockX(Block b, int dx) {
   return true;
 }
 /**
- * @brief test to see if the block can be shifted dy units without leaving the playarea
- * 
- * @param b 
- * @param dy 
- * @return true 
- * @return false 
+ * @brief test to see if the block can be shifted dy units without leaving the
+ * playarea
+ *
+ * @param b
+ * @param dy
+ * @return true
+ * @return false
  */
 bool shiftBlockY(Block b, int dy) {
   // find the lowest point pixel
-  int array[b.num_pixels];
   int y_low_ind = 0;
   int lowy = b.pixels[0]->y;
   for (int i = 0; i < b.num_pixels; i++) {
@@ -55,45 +58,60 @@ bool shiftBlockY(Block b, int dy) {
 }
 /**
  * @brief add the block to the playfield
- * 
- * @param b 
- * @param p 
+ *
+ * @param b
+ * @param p
  */
 void addToPlayfield(Block *b, Playfield *p) {
   int num_block = b->num_pixels;
+  bool gameover = false;
   for (int i = 0; i < num_block; i++) {
     int x = b->pixels[i]->x;
     int y = b->pixels[i]->y;
     Pixel *pix = p->field[x][y];
     // we'll memcopy this instead of copying the register
+    if(y < 0){
+      gameover = true;
+      break;
+    }
     memcpy(pix, b->pixels[i], sizeof(struct pixel));
   }
+  if(gameover){
+    raise(SIGTERM);
+    return;
+  }
   freeBlockElements(b);
+  
+
 }
 // check this after regular bounds checking or it will segfault
 /**
- * @brief check to see if the block collides with the playfield and is in bounds, take the drop value and the dx,dy, and the playfield as a pointer
- * 
- * @param b 
- * @param p 
- * @param dx 
- * @param dy 
- * @param drop 
- * @return true 
- * @return false 
+ * @brief check to see if the block collides with the playfield and is in
+ * bounds, take the drop value and the dx,dy, and the playfield as a pointer
+ *
+ * @param b
+ * @param p
+ * @param dx
+ * @param dy
+ * @param drop
+ * @return true
+ * @return false
  */
 bool playfieldCollisionCheck(Block b, Playfield *p, int dx, int dy, bool drop) {
   if (drop) {
-    int dy_n = 0; /** the new deltay which drops the block the lowest that it can go */
+    int dy_n =
+        0; /** the new deltay which drops the block the lowest that it can go */
     for (int i = 0; i <= HEIGHT; i++) {
       bool out_y = shiftBlockY(b, i);
       if (!out_y) {
-        //if cannot shift block any lower then we save the distance from the bottom to dy_n
-        dy_n = i; 
+        // if cannot shift block any lower then we save the distance from the
+        // bottom to dy_n
+        dy_n = i;
         break;
       }
     }
-    //now that we have the distance from the bottom we want to try and shift it down even more
+    // now that we have the distance from the bottom we want to try and shift it
+    // down even more
     for (int i = 0; i < dy_n; i++) {
       bool pf_col_y = playfieldCollisionCheckY(b, p, i);
       if (!pf_col_y) {
@@ -101,7 +119,7 @@ bool playfieldCollisionCheck(Block b, Playfield *p, int dx, int dy, bool drop) {
         break;
       }
     }
-    //shift the block down as far as it can go
+    // shift the block down as far as it can go
     for (int i = 0; i < b.num_pixels; i++) {
       Pixel *p = b.pixels[i];
       p->y += dy_n - 1;
@@ -111,47 +129,51 @@ bool playfieldCollisionCheck(Block b, Playfield *p, int dx, int dy, bool drop) {
 
   bool out_x = shiftBlockX(b, dx); /** can the block be shifted the dx? */
   bool out_y = shiftBlockY(b, dy); /** can the block be shifted the dy?*/
-  bool out = out_x && out_y; /** can the block be shifted the requested amount?*/
-  bool pf_x /**can the block be shifted on the playfield x?*/, pf_y /**can the block be shifted on the playfield y*/; 
-  //we need to test to see if the block is in bounds or else the program will segfault
+  bool out =
+      out_x && out_y; /** can the block be shifted the requested amount?*/
+  bool pf_x /**can the block be shifted on the playfield x?*/,
+      pf_y /**can the block be shifted on the playfield y*/;
+  // we need to test to see if the block is in bounds or else the program will
+  // segfault
   if (out_x) {
-    //test to see if the block can be shifted on the x axis
+    // test to see if the block can be shifted on the x axis
     pf_x = playfieldCollisionCheckX(b, p, dx);
-    //if the block can be shifted in the desired way we will drop it
+    // if the block can be shifted in the desired way we will drop it
     if (pf_x) {
       for (int i = 0; i < b.num_pixels; i++) {
         Pixel *p = b.pixels[i];
         p->x += dx;
       }
     }
-    //and operation with pf_x and out
+    // and operation with pf_x and out
     out &= pf_x;
   }
-  //we need to test this so the program doesn't segfault
+  // we need to test this so the program doesn't segfault
   if (out_y) {
-    //test to see if the block can be shifted on the y axis
+    // test to see if the block can be shifted on the y axis
     pf_y = playfieldCollisionCheckY(b, p, dy);
-    //if it can then shift it
+    // if it can then shift it
     if (pf_y) {
       for (int i = 0; i < b.num_pixels; i++) {
         Pixel *p = b.pixels[i];
         p->y += dy;
       }
     }
-    //and operation with pf_x and out
+    // and operation with pf_x and out
     out &= pf_y;
   }
-  //return whether or not the block was shifted
+  // return whether or not the block was shifted
   return out;
 }
 /**
- * @brief check to see if the block will intersect with the playfield when shifted horizontally dx blocks
- * 
- * @param b 
- * @param p 
- * @param dx 
- * @return true 
- * @return false 
+ * @brief check to see if the block will intersect with the playfield when
+ * shifted horizontally dx blocks
+ *
+ * @param b
+ * @param p
+ * @param dx
+ * @return true
+ * @return false
  */
 bool playfieldCollisionCheckX(Block b, Playfield *p, int dx) {
   for (int i = 0; i < b.num_pixels; i++) {
@@ -163,13 +185,14 @@ bool playfieldCollisionCheckX(Block b, Playfield *p, int dx) {
   return true;
 }
 /**
- * @brief see if the block b collides with the playfield when shifted down deltaY blocks
- * 
- * @param b 
- * @param p 
- * @param dy 
- * @return true 
- * @return false 
+ * @brief see if the block b collides with the playfield when shifted down
+ * deltaY blocks
+ *
+ * @param b
+ * @param p
+ * @param dy
+ * @return true
+ * @return false
  */
 bool playfieldCollisionCheckY(Block b, Playfield *p, int dy) {
   for (int i = 0; i < b.num_pixels; i++) {
@@ -182,10 +205,10 @@ bool playfieldCollisionCheckY(Block b, Playfield *p, int dy) {
 }
 /**
  * @brief checks to see if the gameover has been achieved
- * 
- * @param b 
- * @return true 
- * @return false 
+ *
+ * @param b
+ * @return true
+ * @return false
  */
 bool gameOverCheck(Block b) {
   bool shift = shiftBlockY(b, 1);
--- src/main.c
+++ src/main.c
@@ -1,155 +1,154 @@
 /* triangle.c */
 
 #include <curses.h>
+#include <locale.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <time.h>
-#include <locale.h>
 #include <unistd.h>
-#include <signal.h>
 
 #include <main.h>
 
-#include <gui.h>
 #include <blocks.h>
 #include <game.h>
+#include <gui.h>
 
 int maxlines, maxcols;
-Playfield* p;
+Playfield *p;
 WINDOW *tetris_win;
 Block b;
 /**
  * @brief handle the ctrl+c from user and clear memory on ctrl-c
- * 
- * @param signum 
+ *
+ * @param signum
  */
-void signal_callback_handler(int signum){
-    endwin();
-    freePlayfield(p);
-    freeBlockElements(&b);
-    exit(0);
+void signal_callback_handler(int signum) {
+  endwin();
+  freePlayfield(p);
+  freeBlockElements(&b);
+  exit(0);
+}
+
+void signal_term_handler(int signum) {
+  endwin();
+  freePlayfield(p);
+  freeBlockElements(&b);
+  // do something for game over eventually
+  exit(0);
 }
 
 /**
  * @brief the main entry point of the program
- * 
+ *
  * @return int return code
  */
-int main(void)
-{
-    signal(SIGINT, signal_callback_handler);
-    curs_set(FALSE);
-    setlocale(LC_ALL, "");
-    srand(time(NULL));
+int main(void) {
+  signal(SIGINT, signal_callback_handler);
+  curs_set(FALSE);
+  setlocale(LC_ALL, "");
+  srand(time(NULL));
 
-    /* initialize curses */
+  /* initialize curses */
 
-    initscr();
-    cbreak();
-    noecho();
+  initscr();
+  cbreak();
+  noecho();
 
-    clear();
+  clear();
 
-    
+  // the update loop
+  tetris_win = draw_gui(0, 0);
+  keypad(tetris_win, true);
+  nodelay(tetris_win, true);
+  cbreak();
+  refresh();
 
-    
-
-    //the update loop
-    tetris_win = draw_gui(0, 0);
-    keypad(tetris_win, true);
-    nodelay(tetris_win, true);
-    cbreak();
-    refresh();
-
-    b = initBlock(blockGenerator());
-    p = initialize_playfield(WIDTH,HEIGHT);
-    bool block_update = true;
-    bool go = false;
-    //loop that updates the screen at a constant rate
-    while(1)
-    {
-        int dx,dy;
-        bool drop = false;
-        int down = wgetch(tetris_win);
-        switch (down)
-        {
-        case KEY_UP:
-            dy = -1;
-            break;
-        case KEY_DOWN:
-            dy = 2;
-            break;
-        case KEY_LEFT:
-            dx = -1;
-            break;
-        case KEY_RIGHT:
-            dx = 1;
-            break;
-        case ' ':
-            drop = true;
-            break;
-        default:
-            dy = 1;
-            dx = 0;
-            break;
-        }
-        tick();
-        update();
-        flushinp();
-        usleep(100000);
-        if(block_update){
-            bool bx = !shiftBlockX(b, dx);
-            bool by = !shiftBlockY(b, dy);
-            
-            if(!bx){
-                if(playfieldCollisionCheckX(b,p,dx)){
-                    if(by || !playfieldCollisionCheckY(b,p,dy)){
-                        if(go){
-                            break;
-                        }
-                        addToPlayfield(&b, p);
-                        b = initBlock(blockGenerator());
-                    }
-                }
-            }
-            block_update = false;
-        }else{
-            block_update = !updateBlock(b, p, dy, dx, drop);
-            if(drop){
-                go = gameOverCheck(b);
-                if(go){
-                    break;
-                }
-                addToPlayfield(&b, p);
-                b = initBlock(blockGenerator());
-            }
-            drop = false;
-        }
-        clear();
+  b = initBlock(blockGenerator());
+  p = initialize_playfield(WIDTH, HEIGHT);
+  bool block_update = true;
+  bool go = false;
+  // loop that updates the screen at a constant rate
+  while (1) {
+    int dx, dy;
+    bool drop = false;
+    int down = wgetch(tetris_win);
+    switch (down) {
+    case KEY_UP:
+      dy = -1;
+      break;
+    case KEY_DOWN:
+      dy = 2;
+      break;
+    case KEY_LEFT:
+      dx = -1;
+      break;
+    case KEY_RIGHT:
+      dx = 1;
+      break;
+    case ' ':
+      drop = true;
+      break;
+    default:
+      dy = 1;
+      dx = 0;
+      break;
     }
-    
-    //something something game over
+    tick();
+    update();
+    flushinp();
+    usleep(100000);
+    if (block_update) {
+      bool bx = !shiftBlockX(b, dx);
+      bool by = !shiftBlockY(b, dy);
 
-    refresh();
+      if (!bx) {
+        if (playfieldCollisionCheckX(b, p, dx)) {
+          if (by || !playfieldCollisionCheckY(b, p, dy)) {
+            if (go) {
+              break;
+            }
+            addToPlayfield(&b, p);
+            b = initBlock(blockGenerator());
+          }
+        }
+      }
+      block_update = false;
+    } else {
+      block_update = !updateBlock(b, p, dy, dx, drop);
+      if (drop) {
+        go = gameOverCheck(b);
+        if (go) {
+          break;
+        }
+        addToPlayfield(&b, p);
+        b = initBlock(blockGenerator());
+      }
+      drop = false;
+    }
+    clear();
+  }
 
-    getch();
-    endwin();
+  // something something game over
 
-    signal_callback_handler(0);
+  refresh();
+
+  getch();
+  endwin();
+
+  signal_callback_handler(0);
 }
 /**
  * @brief the update method that is called to redraw the gui
- * 
+ *
  */
-void update(){
-    maxlines = LINES - 1;
-    maxcols = COLS - 1;
-    draw_gui(0,0);
-    draw_playfield(p);
-    draw_block(b);
+void update() {
+  maxlines = LINES - 1;
+  maxcols = COLS - 1;
+  draw_gui(0, 0);
+  draw_playfield(p);
+  draw_block(b);
 
-    refresh();
+  refresh();
 }
 
-void tick(){
-    
-}
+void tick() {}
